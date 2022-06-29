04e91f6eb40c9c54802a5b5446c619001204f3b0
N Celine <naildbyceline@gmail.com>
2022-06-29 08:09:12 -0400

    second part

--- starter/script.js
+++ starter/script.js
@@ -1,30 +1,58 @@
-let name = prompt("What is your name?");
-alert("Hello, " + name + "!");
-{
-  console.log(prompt);
+// const input = prompt("Welcome to an amazing adventure, What is your name?");
+// alert("Nice to meet you ${input}");
+
+// const choiceAdventure = ('What would be your ideal adventure be, would you choose a Amazonian jungle, cavern of unexplored caves or survive a zombie breakout?') => {
+//   if (amazonJungle === "A");
+//   alert("Grab your backpack and bug repellent!")
+//   else if (cavernCaves === "C");
+//   alert("Did you remember your safety cables?")
+//   if(zombieApocalypse === "Z");
+//   alert("RUNNNNNN and grab your antivirus!")
+// }
+
+//old hw
+
+function startGame() {
+  let name = prompt("What is your name?");
+  alert("Hello, " + name + "!");
+  {
+    console.log(prompt);
+  }
+
+  let exoticTravel = prompt(
+    "Lets take a trip through the 7 continents, you think you have what it takes for an adventure?"
+  );
+  alert("Well come on then, grab your passport");
+
+  let text;
+  let favLocation = prompt(
+    "Whats Continent would you like to start with Africa, South America or Europe?"
+  );
+  switch (favLocation) {
+    case "Africa":
+      alert("Excellent choice! Africa is good for your soul.");
+      break;
+    case "South America":
+      alert("South America is my favorite too!");
+      break;
+    case "Europe":
+      alert("Really? Are you sure the Europe should be first?");
+      break;
+    default:
+      alert("I have never heard of that one!");
+  }
 }
 
-let exoticTravel = prompt(
-  "Lets take a trip through the 7 continents, you think you have what it takes for an adventure?"
-);
-alert("Well come on then, grab your passport");
-
-const friedEggs = prompt("Do you like green eggs and ham lol, just kidding");
-
-let text;
-let favLocation = prompt(
-  "Whats Continent would you like to start with Africa, South America or Europe?"
-);
-switch (favLocation) {
-  case "Africa":
-    text = "Excellent choice! Africa is good for your soul.";
-    break;
-  case "South America":
-    text = "South America is my favorite too!";
-    break;
-  case "Europe":
-    text = "Really? Are you sure the Europe should be first?";
-    break;
-  default:
-    text = "I have never heard of that one!";
+if (chosenLocation === "Africa") {
+  let Africa = prompt("Great choice, Let the safari adventure begin");
+  {
+    if (Europe);
+    let Europe = prompt(
+      "Grab your hats and backpacks, let the treasure hunt begin"
+    );
+    if ("Mayan Temples, here we come!");
+  }
 }
+
+startGame();
+let gameRestart = prompt("Would you like to restart game?");
